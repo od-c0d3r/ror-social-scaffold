46084bc9530d2618975f0caa1130eb63f7f5aec1
tanzila-abedin <tanzila.abedin@yahoo.com>
2021-06-25 23:10:18 +0200

    Fix rubocop error

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -1,5 +1,4 @@
 module FriendshipsHelper
-
   def friendships_collection
     friendships = Friendship.where(user_id: current_user.id, confirmed: true)
     friendships += Friendship.where(friend_id: current_user.id, confirmed: true)
@@ -7,37 +6,35 @@ module FriendshipsHelper
   end
 
   def current_friends(user)
-     friend_arr = []
-     friendships_collection.each do |relation|
-       if relation.user_id == user.id 
-          element = User.find_by(id: relation.friend_id)
-          friend_arr << element
-       else 
-          element2 = User.find_by(id: relation.user_id)
-          friend_arr << element2
-       end
-     end
-   end
+    friend_arr = []
+    friendships_collection.each do |relation|
+      if relation.user_id == user.id
+        element = User.find_by(id: relation.friend_id)
+        friend_arr << element
+      else
+        element2 = User.find_by(id: relation.user_id)
+        friend_arr << element2
+      end
+    end
+  end
 
   def user_friends(user)
-     friend_arr = []
-     friendships_collection.each do |relation|
-       if relation.user_id == user.id 
-          element = User.find_by(id: relation.friend_id)
-          friend_arr << element
-       else 
-          element2 = User.find_by(id: relation.user_id)
-          friend_arr << element2
-       end
-     end
+    friend_arr = []
+    friendships_collection.each do |relation|
+      if relation.user_id == user.id
+        element = User.find_by(id: relation.friend_id)
+        friend_arr << element
+      else
+        element2 = User.find_by(id: relation.user_id)
+        friend_arr << element2
+      end
+    end
   end
 
   def mutual_friend(user1, user2)
-      c_f = current_friends(user1)
-      u_f = user_friends(user2)
-     mutual_arr = c_f & u_f
-     return mutual_num = mutual_arr.count
+    c_f = current_friends(user1)
+    u_f = user_friends(user2)
+    mutual_arr = c_f & u_f
+    mutual_arr.count
   end
-
-
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,23 +1,21 @@
 module UsersHelper
-
   def request_sent?(friend)
     current_user.friendships.where(friend_id: friend.id, confirmed: nil).empty?
   end
 
-  def is_friend?(user)
+  def friend?(user)
     value1 = Friendship.where(user_id: current_user.id, friend_id: user.id, confirmed: true)
     value2 = Friendship.where(user_id: user.id, friend_id: current_user.id, confirmed: true)
-    value1.present? || value2.present? 
+    value1.present? || value2.present?
   end
 
   def gravatar_for(user)
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    return image_tag(gravatar_url, alt: user.name, class: 'gravatar')
+    image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
 
   def me?(user)
     current_user == user
   end
-  
 end
