b1af205d6f023eecbac3048206553d6a4d9ea810
tmampa <tmampa94@gmail.com>
2022-03-04 12:21:50 +0200

    Create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   root 'posts#index'
   get 'friendships/create'
   get 'my_friends', to: 'users#my_friends'
   get 'requests', to: 'users#requests'
 
   devise_for :users
+  namespace :api do
+    namespace :v1 do
+      post :auth, to 'authentication#create'
+      post :register, to: 'registrations#create'
+      resources :posts, only: [:index, :create] do
+        resources :comments, only: [:index, :create]
+      end
+    end
+  end
 
   resources :users, only: [:index, :show]
   resources :friendships, only: [:new, :create, :update, :destroy]
