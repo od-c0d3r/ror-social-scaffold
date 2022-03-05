6085363c40c871ed317045abffb20f57ec3f4615
od-c0d3r <omar_rashad@aol.com>
2022-03-05 17:25:14 +0200

    Api: create comments api endpoints controller

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,23 +1,28 @@
-class Api::V1::CommentsController < ApiController
-  def index
-    @comments = Post.find(params[:post_id]).comments
+module Api
+  module V1
+      class CommentsController < ApiController
+          def index
+              post = Post.find(params[:post_id])
+              @comments = post.comments
+          end
+
+          def create
+            @comment = Comment.new(comment_params)
+            @comment.post_id = params[:post_id]
+            @comment.user = current_user
+
+            if @comment.save
+              head :created
+            else
+              head :no_content
+            end
+          end
+
+          private
+
+          def comment_params
+            params.require(:comment).permit(:content)
+          end
+      end
   end
-
-  def create
-    @comment = Comment.new(comment_params)
-    @comment.post_id = params[:post_id]
-    @comment.user = current_user
-
-    if @comment.save
-      render json: { success: ['Comment posted'] }
-    else
-      render json: { errors: ['Comment failed '] }
-    end
-  end
-
-  private
-
-  def comment_params
-    params.require(:comment).permit(:content)
-  end
-end
+end 
