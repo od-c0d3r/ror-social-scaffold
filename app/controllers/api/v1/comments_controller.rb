d9175399879bd45a31a1870505085ca3e9c3cac0
od-c0d3r <omar_rashad@aol.com>
2022-03-05 10:58:51 +0200

    Api: build the json view for view comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,8 +2,8 @@ module Api
     module V1
         class CommentsController < ApiController
             def index
-                @post = Post.find(params[:post_id])
-                @comments = @post.comments
+                post = Post.find(params[:post_id])
+                @comments = post.comments
             end
         end
     end
