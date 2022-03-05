7dd9f4a4c44cc040d8c81a889af5ce65f3082475
od-c0d3r <omar_rashad@aol.com>
2022-03-05 17:16:38 +0200

    Fix: posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,5 +1,26 @@
-class Api::V1::PostsController < ApiController
-  def index
-    @posts = Post.all
+module Api
+  module V1
+      class PostsController < ApiController
+          def index
+              timeline_posts
+          end
+
+          private
+
+          def timeline_posts
+              @my_posts = current_user.posts
+              @friends = Friendship.where(user_id: current_user.id, confirmed: true)
+
+              @my_friends_posts = []
+              @friends.each do |friendship|
+                user = User.find_by(id: friendship.friend_id)
+                @my_friends_posts << user.posts
+              end
+
+              @timeline_posts = @my_posts + @my_friends_posts
+
+              @timeline_posts.compact
+            end
+      end
   end
-end
+end 
