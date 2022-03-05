b5462a29bb9d1ed39fbc03b081e3416a86c4aa58
od-c0d3r <omar_rashad@aol.com>
2022-03-05 12:13:50 +0200

    Api: comments creation endpoint

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,6 +5,24 @@ module Api
                 post = Post.find(params[:post_id])
                 @comments = post.comments
             end
+
+            def create
+                @comment = Comment.new(comment_params)
+                @comment.post_id = params[:post_id]
+                @comment.user = current_user
+            
+                if @comment.save
+                  head :created
+                else
+                  head :no_content
+                end
+              end
+            
+              private
+            
+              def comment_params
+                params.require(:comment).permit(:content)
+              end
         end
     end
 end
